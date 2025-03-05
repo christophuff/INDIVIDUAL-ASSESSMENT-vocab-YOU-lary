e9e029ab6e3015df4a6e328050e98957f989cad8
christophuff <chris.huff@hotmail.com>
2025-03-04 21:32:33 -0600

    auth fixed

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 // USE WITH FIREBASE AUTH
+import 'firebase/auth';
 import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
